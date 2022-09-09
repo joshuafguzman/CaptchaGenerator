3c1d6246db598666bea0a7b5a6b624ad8d668af2
Joshua Guzman <106280350+joshuafguzman@users.noreply.github.com>
2022-09-09 11:29:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 //Using this to learn
 
 // document.querySelector() is used to select an element from the document using its ID
-let captchaText = document.querySelector('#captcha');
-var ctx = captchaText.getContext("2d");
+let captchaText = document.querySelector('#captcha'); //let defines a varible
+var ctx = captchaText.getContext("2d"); //var also defines a varible
 ctx.font = "30px Roboto";
 ctx.fillStyle = "#08e5ff";
 
@@ -28,12 +28,85 @@ for (let i = 1; i <= 7; i++) {
 var c = emptyArr.join('');
 ctx.fillText(emptyArr.join(''),captchaText.width/4, captchaText.height/2);
 
+//This changes the color of the background when using the click button on the keyboard
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+userText.addEventListener('click', () => {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
+/*
+//When pressing on keybord it alerts what key was pressed.
+userText.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+
+  if (keyName === 'Shift') {
+    // do not alert when only Shift key is pressed.
+    return;
+  }
+if (event.shiftKey) {
+    
+    alert(`Combination of shiftKey + ${keyName}`);
+  } else {
+    alert(`Key pressed ${keyName}`);
+  }
+  
+}, false);
+*/
+
+
+//Use this to count the words per minute and Characters per minute
+$(function () {
+    $(userText)
+        .keyup(checkSpeed);
+});
+
+var iLastTime = 0;
+var iTime = 0;
+var iTotal = 0;
+var iKeys = 0;
+
+function checkSpeed() {
+    iTime = new Date().getTime();
+
+    if (iLastTime != 0) {
+        iKeys++;
+        iTotal += iTime - iLastTime;
+        iWords = $(userText).val().split(/\s/).length;
+        $('#CPM').html(Math.round(iKeys / iTotal * 60000, 2));
+        
+    }
+
+    iLastTime = iTime;
+}
+
+//keydown makes each key that is typed go to the console log
+   document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    let buffer = [];
+
+    document.addEventListener('keydown', event => {
+        const charList = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        const key = event.key.toLowerCase();
+
+        // we are only interested in alphanumeric keys
+        if (charList.indexOf(key) === -1) return;
+
+        buffer.push(key);
+        console.log(buffer);
+    });
+});
+
 
 // This event listener is stimulated whenever the user press the "Enter" button
 // "Correct!" or "Incorrect, please try again" message is
 // displayed after validating the input text with CAPTCHA
 userText.addEventListener('keyup', function(e) {
 	// Key Code Value of "Enter" Button is 13
+
     if (e.keyCode === 13) {
         if (userText.value === c) {
             output.classList.add("correctCaptcha");
@@ -43,7 +116,7 @@ userText.addEventListener('keyup', function(e) {
             output.innerHTML = "Incorrect, please try again";
         }
     }
-});
+},false);
 
 // This event listener is stimulated whenever the user clicks the "Submit" button
 // "Correct!" or "Incorrect, please try again" message is
@@ -71,3 +144,4 @@ refreshButton.addEventListener('click', function() {
     ctx.fillText(refreshArr.join(''),captchaText.width/4, captchaText.height/2);
     output.innerHTML = "";
 });
+
